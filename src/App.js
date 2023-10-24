5db8613840acbbb8a9b9f6a5bf14d7862313b463
Jan Bogosyan <janbogosyan@gmail.com>
2023-10-24 20:35:28 +0300

    upg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
+const questions = [
+  {
+    prompt: "what color is the sky?",
+    correctAnswer: "blue",
+    answers: ["brown", "white", "blue"],
+  }
+]
+
+
 function App() {
+
+  const [currentQuestionIndex, setCurrentQuestionndex] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState("");
+
+  const currentQuestion = questions[currentQuestionIndex];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{currentQuestion.prompt}</h1>
+
+      {selectedAnswer}
+
+      <form onSubmit={(e) => {
+        e.preventDefault();
+
+        // TODO did they get the right answer
+
+        //TODO increment to the next question OR end the gamef they've finished all questions
+
+      }}
+      >
+        {currentQuestion.answers.map((answer) => {
+          return (
+            <label key={answer}>
+              <input
+                onChange={() => {
+                  setSelectedAnswer(answer);
+                }}
+                type='radio'
+                name='answer'>
+              </input>
+              {answer}
+            </label>
+          );
+        })}
+
+        <button>Submit</button>
+      </form>
+
     </div>
   );
 }
